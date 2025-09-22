a8dc910c6b0e1dc0493695ba23e3961078a950bb
Ahmad Alam <ahmadfalam@gmail.com>
2025-09-22 14:08:47 +0100

    Refactor setup.py for headsup-fast package
    Updated package details and classifiers for headsup-fast.

--- setup.py
+++ setup.py
@@ -1,39 +1,45 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
 from setuptools import setup, find_packages
-from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(
-    name='headsup',
-    version='0.1.1',
-    author='Tarjei Husøy',
-    author_email='git@thusoy.com',
-    url='https://github.com/thusoy/headsup',
-    description="Lightweight alternative to Ubuntu's landscape-sysinfo",
-    py_modules=['headsup'],
-    zip_safe=False,
-    entry_points={
-        'console_scripts': [
-            'headsup = headsup:main'
-        ]
-    },
+    name="headsup-fast",
+    version="1.0.0",
+    author="Ahmad Alam",
+    author_email="ahmadalam@outlook.com",
+    description="A trivial alternative to Ubuntu's landscape-info with no external dependencies",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ahmadalam/headsup-fast",  # Replace with your GitHub repo
+    py_modules=["headsup"],
     classifiers=[
-        # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'Intended Audience :: System Administrators',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: Unix',
-        'Operating System :: POSIX',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'Topic :: Utilities',
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Topic :: System :: Systems Administration",
+        "Topic :: System :: Monitoring",
     ],
+    python_requires=">=3.6",
+    entry_points={
+        "console_scripts": [
+            "headsup=headsup:main",
+        ],
+    },
+    keywords="system monitoring motd landscape ubuntu linux sysinfo",
+    project_urls={
+        "Bug Reports": "https://github.com/ahmadalam/headsup-fast/issues",
+        "Source": "https://github.com/ahmadalam/headsup-fast/headsup.py",
+    },
 )
